64fd1c58f153cb4b220dd2b9a1d608c0df1c41d3
Poyraz <78071274+poyrazavsever@users.noreply.github.com>
2026-07-06 22:52:49 +0300

    chore: update turbo.json for api and add proxy in app

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -17,6 +17,14 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: "http://localhost:3002/api/v1/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
