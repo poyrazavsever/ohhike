5c6d8f5880e903803da56c4cedaf3ecd53309910
Poyraz <78071274+poyrazavsever@users.noreply.github.com>
2026-07-06 22:52:58 +0300

    refactor(web): remove obsolete coach network routes

--- apps/web/app/(coach-network)/account-type/page.tsx
+++
@@ -1,8 +0,0 @@
-import { AccountTypePicker } from "./_components/account-type-picker";
-import { resolveAccountTypeRedirect } from "./actions";
-
-export default async function AccountTypePage() {
-  await resolveAccountTypeRedirect();
-
-  return <AccountTypePicker />;
-}
